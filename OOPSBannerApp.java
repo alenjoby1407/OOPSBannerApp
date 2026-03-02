42c4ed7365e97ddf14bbf709319c4b4b45fc4778
alenjoby1407 <alenjoby1407@gmail.com>
2026-03-02 23:02:32 +0530

    Implemented UC2: Banner using + operator

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,20 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display using + operator
+ *
+ * Prints OOPS in 7-line banner format using
+ * string concatenation.
+ */
+
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println("  ***   " + "  ***   " + " ****** " + " ***** ");
+        System.out.println(" *   *  " + " *   *  " + " *    * " + " *     ");
+        System.out.println(" *   *  " + " *   *  " + " *    * " + " *     ");
+        System.out.println(" *   *  " + " *   *  " + " ****** " + " ****  ");
+        System.out.println(" *   *  " + " *   *  " + " *      " + "     * ");
+        System.out.println(" *   *  " + " *   *  " + " *      " + " *   * ");
+        System.out.println("  ***   " + "  ***   " + " *      " + "  ***  ");
     }
 }
